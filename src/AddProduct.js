6a71227397776c255283011416861c9ec0771d86
rominalodolo <lodoloromina@gmail.com>
2021-12-01 12:50:32 +0200

    readme

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -5,12 +5,15 @@ class AddProduct extends Component {
     productName: "",
     productPrice: 0,
   };
+
   productNameChangedHandler = (event) => {
     this.setState({ productName: event.target.value });
   };
+
   productPriceChangedHandler = (event) => {
     this.setState({ productPrice: event.target.value });
   };
+  
   render() {
     return (
       <div className="container">
